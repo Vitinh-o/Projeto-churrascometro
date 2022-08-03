1ef8c0a75acdcecc1d8486ef6c66769cd5adf2e3
Victor Emanuel de Souza Ferreira <victorem@ccuec.unicamp.br>
2022-08-03 14:26:25 -0300

    Fo iniciado o projeto e adicionado a pasta imagens

---
+++ script.js
@@ -0,0 +1,13 @@
+ /* Adultos (por pessoa):
+Quantidade de carne para homens: 400 gramas
+Quantidade de carne para mulheres: 300 gramas
+Acompanhamentos: 200 gramas
+Quantidade de bebida não alcoólica (refrigerante, suco e água): 1,5 litro
+Quantidade de cerveja: 5 latas ou long necks
+
+Crianças de até 11 anos (por pessoa):
+Quantidade de carne: 200 gramas
+Quantidade de bebida não alcoólica (refrigerante, suco e água): 750 ml
+
+Carvão: 5 quilos para 6 quilos de carne */
+
