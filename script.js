840ec71aed531d455875bc80960cbf779e5fbfb0
Victor Emanuel de Souza Ferreira <victorem@ccuec.unicamp.br>
2022-08-04 11:30:51 -0300

    Foi inserido um estilo ao conteúdo, foi realizado um trabalhor em ralação a responsividade.

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ Quantidade de carne para homens: 400 gramas
 Quantidade de carne para mulheres: 300 gramas
 Acompanhamentos: 200 gramas
 Quantidade de bebida não alcoólica (refrigerante, suco e água): 1,5 litro
-Quantidade de cerveja: 5 latas ou long necks
+Quantidade de cerveja: 1200ml
 
 Crianças de até 11 anos (por pessoa):
 Quantidade de carne: 200 gramas
 Quantidade de bebida não alcoólica (refrigerante, suco e água): 750 ml
 
 Carvão: 5 quilos para 6 quilos de carne */
+
+
 
